0321a3db5ce1bfa3b874d3d4c0e160f6eca2db0b
Sean Kelly <skelly215@gmail.com>
2023-06-18 20:50:00 -0400

    new approach

--- script.js
+++ script.js
@@ -1,84 +1,73 @@
 // snak: classic snake game
 
 const canvas = document.querySelector('canvas');
-
 const field = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-var d = 'dU';
-
-function getDir() {
-  document.addEventListener("keydown", function(event) {
-    if (event.key == "ArrowUp"){
-      snak.frame('dU');
-    } else if (event.key == "ArrowRight"){
-      snak.frame('dR');
-    } else if (event.key == "ArrowDown"){
-      snak.frame('dD');
-    } else if (event.key == "ArrowLeft"){
-      snak.frame('dL');
-    } else alert('derp');
-  });
-}
 
 class Snak {
 
   constructor() {
-    this.position = {
-      x: canvas.width/2,
-      y: canvas.height/2
+      this.pos = {
+        x: canvas.width/2,
+        y: canvas.height/2
+      }
+      this.vel = {
+        x: 0,
+        y: 0
+      }
+      this.width = 10;
+      this.height = 10;
+      this.perp = true;
     }
-    this.vel = {
-      x: 0,
-      y: 0
+
+    draw() {
+      field.fillStyle = 'yellow';
+      field.fillRect(
+        this.pos.x,
+        this.pos.y,
+        this.width,
+        this.height
+      )
     }
-    this.width = 10;
-    this.height = 10;
-  }
-  draw() {
-    field.fillStyle = 'yellow';
-    field.fillRect(
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    )
 
+    frame() {
+      advHead();
+      this.draw();
   }
-
-  frame(d) {
-    switch (d) {
-      case 'dU': {this.position.y -= this.width; break;}
-      case 'dD': {this.position.y += this.width; break;}
-      case 'dL': {this.position.x -= this.height; break;}
-      case 'dR': {this.position.x += this.height; break;}
-    }
-    this.draw();
-  }
-
 }
 
 const snak = new Snak();
 
+function advHead() {
+  switch(snak.orig) {
+    case 'N': snak.pos.y -= snak.height;
+    case 'E': snak.pos.x += snak.height
+    case 'S': snak.pos.y += snak.height
+    case 'W': snak.pos.x -= snak.height
+  }
+
+}
+
 function step() {
   requestAnimationFrame(step);
-  // field.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+  snak.frame();
+  console.log(snak.ori);
 
-  document.addEventListener("keydown", function(event) {
-    if (event.key == "ArrowUp"){
-      d = 'dU';
-    } else if (event.key == "ArrowRight"){
-      d = 'dR';
-    } else if (event.key == "ArrowDown"){
-      d = 'dD';
-    } else if (event.key == "ArrowLeft"){
-      d = 'dL';
-    } else snak.frame(d);
-  });
+  // document.addEventListener("keydown", function(event) {
+  //   if (snak.orig === 'E' || snak.orig === 'W') {
+  //     switch(event.key) {
+  //       case "ArrowUp": { snak.ori = 'N'; break;}
+  //       case "ArrowDown": { snak.ori = 'S'; break;}
+  //     }
+  //   } else if (snak.orig === 'N' || snak.orig === 'S') {
+  //     switch(event.key) {
+  //       case "ArrowLeft": { snak.ori = 'W'; break;}
+  //       case "ArrowRight": { snak.ori = 'E'; break;}
+  //     }
+  //   }
+  // });
 
-  snak.frame(d);
 }
 
 step();
