5cdd245e692dc525befda12b28d9920240417df6
Sean Kelly <skelly215@gmail.com>
2023-06-20 19:33:29 -0400

    walls 50% done

--- snak.js
+++ snak.js
@@ -1,42 +1,59 @@
-// snak: classic snake game
-
+// snak: classic snake game :P
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const grid = canvas.getContext('2d');
+
+// derive a grid 100 units wide by however many square units fit vertically
 const numCols = 100;
 const numRows = Math.floor(numCols*(canvas.height/canvas.width));
-const tileSize = Math.floor(canvas.width/numCols);
-let tile = new Array(numCols).fill((new Array(numRows).fill(null)));
+const tileSize = Math.floor(canvas.width/numCols); //in px
 
+// 2d state index[x][y] === xy coords of tile
+let tile = new Array(numCols).fill((
+            new Array(numRows).fill(
+              null
+            )));
+
+// draw/redraw tile
 function draw(x,y,color) {
   grid.fillStyle = color;
   grid.fillRect(x, y, tileSize, tileSize);
 }
 
+// 100 x _ tiles  (draw clean grid)
 function drawBoard() {
-  [...Array(numCols)].forEach((_, i) => {
-    [...Array(numRows)].forEach((_, p) => {
-      draw(i*tileSize,p*tileSize,'gray');
+  [...Array(numCols)].forEach((_, x) => {
+    [...Array(numRows)].forEach((_, y) => {
+      draw(x*tileSize,y*tileSize,'gray');
     })
   });
 }
 
+// all edge tiles from null to '0' (walls turn solid)
 function drawWalls(){
-  tile.forEach((_, i) => {
-    draw(i*tileSize,0,'red');
-    tile[i][0] = 0;
 
-    draw(i*tileSize, numRows*tileSize-tileSize,'purple');
-    tile[i][numRows] = 0;
+  // top and bottom walls
+  tile.forEach((_, x) => {
+    draw(x*tileSize,0,'red');
+    console.log(" red tiles")
+    tile[x][0] = 0; // works
+
+    draw(x*tileSize, numRows*tileSize,'purple');
+    //console.log(" purple tile")
+    tile[x][numRows] = 0; // works
   });
 
-  tile[0].forEach((_, i) => {
-    draw(0, i*tileSize-tileSize,'blue');
-    //tile[0][i] = 0;
+  // left wall
+  tile[0].forEach((_, y) => {
+    draw(0, y*tileSize,'blue');
+    //tile[0][y] = 0;
+  });
 
-    draw(numCols*tileSize, i*tileSize-tileSize,'green');
-    //tile[numCols][i] = 0;
+  // right wall
+  tile[numRows].forEach((_, y) => {
+    draw(numCols*tileSize, y*tileSize,'green');
+    //tile[numCols][y] = 0;
   });
 }
 
