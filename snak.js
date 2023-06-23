01dfd53675afa5d7fe41197489a09869212cc79f
Sean Kelly <skelly215@gmail.com>
2023-06-23 19:48:02 -0400

    cleanup

--- snak.js
+++ snak.js
@@ -1,94 +1,85 @@
-
-const canvas = document.querySelector('canvas');
-canvas.width = (window.innerWidth);
-canvas.height = window.innerHeight;
-const grid = canvas.getContext('2d');
-const numCols = 100;
-const numRows = Math.floor(numCols*(canvas.height/canvas.width));
-const tileSize = Math.floor(canvas.width/numCols); //in px
+const view = document.querySelector('canvas');
+view.width = window.innerWidth; view.height = window.innerHeight;
+const grid = view.getContext('2d');
+const cols = 100; const rows = Math.floor(cols*(view.height/view.width));
+const size = Math.floor(view.width/cols);
 let axis = true;
 let face = 'n';
-let head = [Math.floor(numCols/2), Math.floor(numRows/2)]
-
-let tile = new Array(numCols).fill(null).map(() => (new Array(numRows).fill(null)));
-
-function drawtile(x,y,color) {
-  grid.fillStyle = color;
-  grid.fillRect(x, y, tileSize, tileSize);
-}
-
-function initBoard() {
-  [...Array(numCols)].forEach((_, x) => {
-    [...Array(numRows)].forEach((_, y) => {
-      drawtile(x*tileSize,y*tileSize,'black');
-    })
-  });
-}
-
-function initWalls(){
+let head = [Math.floor(cols/2), Math.floor(rows/2)];
+let tail = [];
+let tile = new Array(cols).fill(null).map(() => (new Array(rows).fill(null)));
+tile[head[0]][head[1]] = 1;
+function board(){
+  grid.fillStyle = '#110';
+  grid.fillRect(0, 0, view.width, view.height);
   tile.forEach((_, x) => {
-    drawtile(x*tileSize,0,'gray');
+    draw(x*size,0,'#665');
     tile[x][0] = 0;
-    drawtile(x*tileSize, numRows*tileSize,'gray');
-    tile[x][numRows] = 0;
+    draw(x*size, rows*size,'#665');
+    tile[x][rows] = 0;
   });
   tile[0].forEach((_, y) => {
-    drawtile(0, y*tileSize,'gray');
+    draw(0, y*size,'#665');
     tile[0][y] = 0;
   });
-  tile[numRows].forEach((_, y) => {
-    drawtile(numCols*tileSize, y*tileSize,'gray');
-    tile[numCols-1][y] = 0;
+  tile[rows].forEach((_, y) => {
+    draw(cols*size, y*size,'#665');
+    tile[cols-1][y] = 0;
   });
 }
-
-function setHead() {
-  tile[head[0]][head[0]] = 0; // set first tile to solid
-  drawtile(head[0]*tileSize, head[1]*tileSize, 'yellow'); // draw first tile
+function draw(x,y,color) {
+  grid.fillStyle = color;
+  grid.fillRect(x, y, size, size);
 }
-
-function startGame() {
-  initBoard();
-  initWalls();
-  setHead();
-  turn();
+function drawHead() {
+  tail.push(head);
+  draw(head[0]*size, head[1]*size, '#990');
 }
-
-function evalHead() {
-  console.log(tile[head[0]][head[1]]);
-  // if null, all good
-  // if 0, game over
-  //if 1, skip tail clear
-}
-
-function turn() {
-  document.addEventListener("keydown", function(dir) {
-    if (axis)  {
-      switch(dir.key) {
-        case "ArrowLeft": { axis = false; face = 'w'; break;}
-        case "ArrowRight": { axis = false; face = 'e'; break;}
-      }
-    } else {
-      switch(dir.key) {
-        case "ArrowUp": { axis = true; face = 'n'; break;}
-        case "ArrowDown": { axis = true; face = 's'; break;}
-      }
-    }
-  });
-
-  switch(face) {
+function advHead(){
+  tile[head[0]][head[1]] = 0;
+  switch (face) {
     case 'w': { head[0] -= 1; break; }
     case 'e': { head[0] += 1; break; }
     case 'n': { head[1] -= 1; break; }
     case 's': { head[1] += 1; break; }
   }
-
-  evalHead();
-  drawtile(head[0]*tileSize, head[1]*tileSize, 'yellow');
-
-  setTimeout( () => {
-    requestAnimationFrame(turn);
-  }, 48);
 }
-
-startGame();
+function remTail(){
+  draw(tail[0][0]*size, tail[0][1]*size, '#110');
+  tile[tail[0][0]][tail[0][1]] = null;
+  tail.shift();
+  console.log(`${tail}`)
+  advHead();
+}
+function evalHead(h) {
+  h !== 0 ?
+    h > 0 ? 
+      advHead()
+      : remTail()
+    : location.reload();
+}
+function start() {
+  board();
+  turn();
+}
+function turn() {
+  document.addEventListener("keydown", function(dir) {
+    if (axis)  {
+      switch(dir.key) {
+        case "ArrowLeft": { axis = false; face = 'w'; break; }
+        case "ArrowRight": { axis = false; face = 'e'; break; }
+        }
+      } else {
+      switch(dir.key) {
+        case "ArrowUp": { axis = true; face = 'n'; break; }
+        case "ArrowDown": { axis = true; face = 's'; break; }
+        }
+      }
+    });
+    evalHead(tile[head[0]][head[1]]);
+    drawHead(tile[head[0]][head[1]]);
+    setTimeout( () => {
+    requestAnimationFrame(turn);
+  }, 500);
+}
+start();
