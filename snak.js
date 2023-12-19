af59e1caa408bda769405417d91ce0351481c88e
Sean Kelly <skelly215@gmail.com>
2023-12-18 23:29:42 -0500

    color

--- snak.js
+++ snak.js
@@ -11,36 +11,39 @@ let head = [Math.floor(cols / 2), Math.floor(rows / 2)];
 let tail = [];
 let bearing = [1,0];
 let snak;
-let velo = 200;
+let velo = 150;
 let tile = new Array(cols).fill(null).map(() => new Array(rows).fill(null));
 
 function drawBoard() {
-    grid.fillStyle = "#111";
+    grid.fillStyle = "#1b1c18";
     grid.fillRect(0, 0, cols*size+size, rows*size+size);
     tile.forEach((_, x) => {
       tile[x][0] = 0;
-      draw(x * size, 0, "#222");
+      draw(x * size, 0, "#472e27");
       tile[x][rows] = 0;
-      draw(x * size, rows * size, "#222");
+      draw(x * size, rows * size, "#472e27");
     });
     tile[0].forEach((_, y) => {
       tile[0][y] = 0;
-      draw(0, y * size, "#222");
+      draw(0, y * size, "#472e27");
     });
     tile[cols-1].forEach((_, y) => {
       tile[cols-1][y] = 0;
-      draw(cols * size, y * size, "#222");
+      draw(cols * size, y * size, "#472e27");
     });
     drawHead();
 }
 
 function draw(x, y, color) {
+  // grid.shadowBlur = 5;
+  // grid.shadowColor = color;
   grid.fillStyle = color;
   grid.fillRect(x, y, size, size);
+  // grid.fillRect(x,y,size,size);
 }
 
 function drawHead() {
-  draw(head[0] * size, head[1] * size, "#070");
+  draw(head[0] * size, head[1] * size, "#757a4b");
 }
 
 function advHead() {
@@ -50,7 +53,7 @@ function advHead() {
 function remTail() {
   tail.push([...head]);
   tile[tail[0][0]][tail[0][1]] = null;
-  draw(tail[0][0] * size, tail[0][1] * size, "#111");
+  draw(tail[0][0] * size, tail[0][1] * size, "#1b1c18");
   !snak ? tail.shift() : drawSnak();
 }
 
@@ -66,8 +69,8 @@ function drawSnak() {
   let x = Math.floor(Math.random()*(cols-2)+1);
   let y = Math.floor(Math.random()*(rows-2)+1);
   tile[x][y] = 1;
-  draw(x * size, y * size, "#500");
-  velo -= 10;
+  draw(x * size, y * size, "#30823c");
+  velo -= 5;
 }
 
 function turn() {
